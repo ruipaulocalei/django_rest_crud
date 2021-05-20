044065b403cfed499ab37aa756581f6685d8d87b
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-05-20 05:45:40 +0100

    Model Added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    description = models.TextField(blank=True, null=True, default='')
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    countInStock = models.IntegerField(default=0)
